b87b2f28af99253566bf471a21b0b4ed47d07ccc
BrentSousa <brentsousa507@gmail.com>
2024-11-25 17:43:06 -0800

    Phase 6
    Hangman State

--- scripts/Hangman-Game.js
+++ scripts/Hangman-Game.js
@@ -13,7 +13,7 @@ const words = [
 
 document.addEventListener("DOMContentLoaded", () => {
     const lettersContainer = document.getElementById("letters");
-
+    const hangmanImage = document.getElementById("hangman-img");
 
 });
 
@@ -29,4 +29,21 @@ function generateLetterButton() {
     button.addEventListener("click", () => handleGuess(letter, button));
     lettersContainer.appendChild(button);
     }
+}
+
+const hangmanState = {
+    maxGuesses: 6,
+    currentImage: 0,
+    reset() {
+        this.currentImage=0
+        updateHangmanImage();
+    },
+    increment() {
+        this.currentImage++;
+        updateHangmanImage();
+    },
+};
+
+function updateHangmanImage() {
+    hangmanImage.src = `../hangman-game-images/hangman-${hangmanState.currentImage}.svg`;
 }

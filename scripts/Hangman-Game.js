7d7f315a59a0d452e8ae8e218608efc4be0301e0
BrentSousa <brentsousa507@gmail.com>
2024-11-25 17:21:47 -0800

    Phase 4
    Words

---
+++ scripts/Hangman-Game.js
@@ -0,0 +1,9 @@
+// Word List
+
+const words = [
+    { word: "javascript", hint: "A programming language" },
+    { word: "browser", hint: "Used to surf the web" },
+    { word: "keyboard", hint: "Input device" },
+    { word: "developer", hint: "Software creator" },
+    { word: "function", hint: "Code block that does something" },
+];

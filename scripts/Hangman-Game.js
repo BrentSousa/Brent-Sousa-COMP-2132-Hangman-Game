2db6b14f1ca746b52537d02c2c7be7e5ed6567e6
BrentSousa <brentsousa507@gmail.com>
2024-11-25 17:35:26 -0800

    Phase 5
    Generate letters

--- scripts/Hangman-Game.js
+++ scripts/Hangman-Game.js
@@ -6,4 +6,27 @@ const words = [
     { word: "keyboard", hint: "Input device" },
     { word: "developer", hint: "Software creator" },
     { word: "function", hint: "Code block that does something" },
+    { word: "Mouse", hint: "Used to navigate the desktop" },
 ];
+
+// Game function
+
+document.addEventListener("DOMContentLoaded", () => {
+    const lettersContainer = document.getElementById("letters");
+
+
+});
+
+
+generateLetterButton();
+
+function generateLetterButton() {
+    lettersContainer.innerhtml = "";
+    for (let i = 65; i <= 90; i++) {
+    const letter = String.fromCharCode(i);
+    const button = document.createElement("button");
+    button.textContent = letter;
+    button.addEventListener("click", () => handleGuess(letter, button));
+    lettersContainer.appendChild(button);
+    }
+}
